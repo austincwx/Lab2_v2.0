afa03d6c3a5de9dc5bd503f4fc1a82a7cc271a2f
austin <austinchuawx.22@ichat.sp.edu.sg>
2023-06-26 02:15:47 +0800

    bmi done

---
+++ bmi.py
@@ -0,0 +1,17 @@
+def calculate_bmi(height, weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    print(bmi)
+
+    if bmi < 18.5:
+        print("Under Weight")
+        return -1
+    elif bmi >= 18.5 and bmi <= 25.0:
+        print("Normal Weight")
+        return 0
+    elif bmi > 25.0:
+        print("Over Weight")
+        return 1
+    
+calculate_bmi(height=1.73, weight=57.0)
